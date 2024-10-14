5c856033280f78f32fd5e024860a064bc39d4a42
Edgar Parucho <e.parucho@outlook.com>
2024-10-14 17:46:11 -0400

    Fixes date prop validation on habit creation (LH-09ec).

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -6,6 +6,16 @@ const createHabitSchema = {
       (value) => value.length > 0 && value.length <= 30,
     ],
   },
+  date: {
+    isMandatory: false,
+    validations: [
+      (value) => typeof value == 'string',
+      (value) => {
+        const date = new Date(value);
+        return !isNaN(date.getTime());
+      },
+    ],
+  },
 };
 
 module.exports = {
